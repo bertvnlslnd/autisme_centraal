d653e6130c4d9205684b066b2d5690d55d92f373
JerVa94 <jeroen.vanelslande@gmail.com>
2023-01-22 09:31:49 +0100

    fix snel bewegen

--- groenBlokken.js
+++ groenBlokken.js
@@ -32,18 +32,21 @@ function ledsKleuren(e)
 
     var j=0;
 	//console.log("i:"+ value);
+    console.log("ledsAan:")
 	for(j = 1; j <= value; j++)
 	{
 		$("#led"+kolom+j).removeClass("led");
 		$("#led"+kolom+j).addClass("ledOn");
-		//console.log(`#led1${j}`);			
+		console.log("#led"+kolom+j);			
     }	
 	//console.log(j);
 	if(j!=11){
+        console.log("leds uit");
 		for(var k = value+1;k <= 10; k++)
 		{
-	    	$("#led"+kolom+j).addClass("led");
-			$("#led"+kolom+j).removeClass("ledOn");
+	    	$("#led"+kolom+k).addClass("led");
+			$("#led"+kolom+k).removeClass("ledOn");
+            console.log("#led"+kolom+k);
 		}
 	}
 
